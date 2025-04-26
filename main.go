bed8a82a7bcdbaa3639205b26330dc6a4a56769d
una <una@maicoin.com>
2025-04-26 23:27:12 +0800

    *: add domain

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/web.html")
+	})
+
 	http.HandleFunc("/generate-pdf", handler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":5566", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
